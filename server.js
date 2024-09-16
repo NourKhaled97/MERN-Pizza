80c023d3dd18cf0c4f1974e7b700214bf407095f
Nour <nourakhaled.teleng@gmail.com>
2024-09-16 09:39:32 +0300

    make some changes for deployment, run build

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require("./db");
 const PizzasRoute = require("./routes/pizzasRoute");
 const UsersRoute = require("./routes/usersRoute");
 const OrdersRoute = require("./routes/ordersRoute");
+const path = require("path");
 
 const app = express();
 
@@ -15,18 +16,21 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Server working ");
-});
-
 app.use("/api/pizzas", PizzasRoute);
 app.use("/api/users", UsersRoute);
 app.use("/api/orders", OrdersRoute);
 
-// if (process.env.MODE_ENV === "production") {
-//   // tell the server what is the entry point of client (Frontend)
-//   app.use("/", express.static("client/build"));
-// }
+if (process.env.MODE_ENV === "production") {
+  // tell the server what is client folder
+  app.use("/", express.static("client/build"));
+
+  // tell the server what is the entry point of client (Frontend)
+  // "*" means all the request
+  app.get("*", (req, res) => {
+    // sendFile take 2 params: dirname and the path of index folder in client
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
